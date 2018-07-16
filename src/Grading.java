1c0f45ef14ce360cf020491bebb0c3fe281a7e65
HyunsooSong <41133015+HyunsooSong@users.noreply.github.com>
2018-07-16 20:54:49 +0900

    Add files via upload

--- src/Grading.java
+++ src/Grading.java
@@ -1,17 +1,25 @@
-public class Grading {
-    public static void main (String[] args){
-        int point = 81;
-
-        if(point < 20){
+class Grade {
+    int point;
+    public Grade(int point) {
+        this.point = point;
+    }
+    public void grade2() {
+        if (point < 20) {
             System.out.println("F");
-        } else if (point < 40){
+        } else if (point < 40) {
             System.out.println("D");
-        } else if (point < 60){
+        } else if (point < 60) {
             System.out.println("C");
-        } else if (point < 80){
+        } else if (point < 80) {
             System.out.println("B");
         } else {
             System.out.println("A");
         }
+    }
+}
+public class Grading {
+    public static void main (String[] args) {
+        Grade grade = new Grade(81);
+        grade.grade2();
     }
 }
